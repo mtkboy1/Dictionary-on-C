d8cb14b784d70e865821a9a370b1734137141e42
mtkboy1 <mrbrawler42@gmail.com>
2025-03-14 20:22:52 +0300

    modified:   dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -4,13 +4,14 @@ struct dictionary
 {
     int *keys;
     int *vals;
-    int size;; //element to add id
+    int *size;; //element to add id
 };
 struct dictionary create(){
     struct dictionary d;
     d.size = 0;
     d.keys = (int*) calloc(1,sizeof(int*));
     d.vals = (int*) calloc(1,sizeof(int*)); 
+    d.size = (int*) malloc(sizeof(int));
     for(int i=0; i<1; i++){
         d.keys[i] = 1;
         d.vals[i] = 1;
@@ -19,13 +20,13 @@ struct dictionary create(){
 }
 void add(struct dictionary *d, int key, int val){
     d->size++;
-    d->keys = (int*) realloc(d->keys,(d->size+1)*sizeof(int));
-    d->vals = (int*) realloc(d->keys,(d->size+1)*sizeof(int));
+    d->keys = (int*) realloc(d->keys,(*d->size+1)*sizeof(int));
+    d->vals = (int*) realloc(d->keys,(*d->size+1)*sizeof(int));
     d->keys[d->size] = key; 
     d->vals[d->size] = val;
 }
 int get(struct dictionary *d, int key){
-    for (size_t i = 0; i < d->size; i++)
+    for (size_t i = 0; i < *d->size; i++)
     {
         if(d->keys[i]==key){
             return d->vals[i];
@@ -33,10 +34,10 @@ int get(struct dictionary *d, int key){
     }
 }
 void rm(struct dictionary *d, int key){
-    for (size_t i = 0; i < d->size; i++)
+    for (size_t i = 0; i < *d->size; i++)
     {
         if(d->keys[i]==key){
-            for (size_t e = i; e < d->size-1; e++){
+            for (size_t e = i; e < *d->size-1; e++){
                 d->keys[e] = d->keys[e+1];
                 d->vals[e] = d->vals[e+1];
             }
