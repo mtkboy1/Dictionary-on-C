95ed707dca7a25888fca2cd8c5aa4d0a35bbdb45
John Doe <johndoe@email.com>
2025-03-14 15:17:02 +0300

    modified:   dictionary.c 	modified:   dictionary.h

--- dictionary.c
+++ dictionary.c
@@ -6,17 +6,6 @@ struct dictionary
     int *vals;
     int size;; //element to add id
 };
-/*struct dictionary create(size_t size){
-    struct dictionary d;
-    d.thisVal = 0;
-    d.keys = (int*) calloc(size,sizeof(int*));
-    d.vals = (int*) calloc(size,sizeof(int*)); 
-    for(int i=0; i<size; i++){
-        d.keys[i] = 0;
-        d.vals[i] = 0;
-    }
-    return d;
-}*/
 struct dictionary create(){
     struct dictionary d;
     d.size = 0;
@@ -42,5 +31,17 @@ int get(struct dictionary *d, int key){
             return d->vals[i];
         }
     }
-    
+}
+void remove(struct dictionary *d, int key){
+    for (size_t i = 0; i < d->size; i++)
+    {
+        if(d->keys[i]==key){
+            for (size_t e = i; e < d->size-1; e++){
+                d->keys[e] = d->keys[e+1];
+                d->vals[e] = d->vals[e+1];
+            }
+            d->keys = (int*) realloc(d->keys,(d->size+1)*sizeof(int));
+            d->vals = (int*) realloc(d->keys,(d->size+1)*sizeof(int));
+        }
+    }
 }
--- dictionary.h
+++ dictionary.h
@@ -8,3 +8,4 @@ struct dictionary;
 extern struct dictionary create();
 extern void add(struct dictionary *d, int key, int val);
 extern int get(struct dictionary *d, int key);
+extern void remove(struct dictionary *d, int key);
