93fae0bbd30638ee46b698f9e5c1aa58a0c7ea12
mtkboy1 <mrbrawler42@gmail.com>
2025-03-14 20:51:29 +0300

    modified:   dictionary.c 	modified:   dictionary.h 	modified:   main.c

--- main.c
+++ main.c
@@ -6,7 +6,8 @@ int main(){
     struct dictionary d = create();
     add(&d,5,5);
     rm(&d,5);
-   // free(d.keys);
-   // free(d.vals);
+    free(d.keys);
+    free(d.vals);
+    free(d.size);
     return 0;
 }
