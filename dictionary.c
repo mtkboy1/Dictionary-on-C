12a0f37e0eeb64a2d4d06245ab5aad4fa77f6167
mtkboy1 <mrbrawler42@gmail.com>
2025-03-15 14:47:10 +0300

    modified:   dictionary.c 	modified:   dictionary.h 	modified:   main.c

--- dictionary.c
+++ dictionary.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 struct dictionary
 {
-    char *keys;
+    int *keys;
     int *vals;
     int size; //element to add id
 };
@@ -17,14 +17,14 @@ struct dictionary create(){
     }
     return d;
 }
-void insert(struct dictionary *d, char *key, int val){
+void insert(struct dictionary *d, int key, int val){
     d->keys = (int*) realloc(d->keys,(d->size+1)*sizeof(int));
     d->vals = (int*) realloc(d->keys,(d->size+1)*sizeof(int));
     d->keys[d->size] = key; 
     d->vals[d->size] = val;
-    printf("%i",d->size);
+    //printf("%i size\n",d->size);
 }
-int get(struct dictionary *d, char *key){
+int get(struct dictionary *d, int key){
     for (size_t i = 0; i < d->size; i++)
     {
         if(d->keys[i]==key){
@@ -32,10 +32,11 @@ int get(struct dictionary *d, char *key){
         }
     }
 }
-void rm(struct dictionary *d, char *key){
+void rm(struct dictionary *d, int key){
     for (size_t i = 0; i < d->size; i++)
     {
         if(d->keys[i]==key){
+            //printf("%i\n",d->keys[1]);
             for (size_t e = i; e < d->size-1; e++){
                 d->keys[e] = d->keys[e+1];
                 d->vals[e] = d->vals[e+1];
@@ -44,7 +45,6 @@ void rm(struct dictionary *d, char *key){
             d->vals = (int*) realloc(d->keys,(d->size-1)*sizeof(int));
         }
     }
-    printf("%i",d->keys[0]);
 }
 void doFree(struct dictionary *d){
     free(d->keys);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(){
     struct dictionary d = create();
     insert(&d,5,5);
     insert(&d,6,5);
-    rm(&d,5);
+    rm(&d,1);
     doFree(&d);
     //free(d.keys);
     //free(d.vals);
